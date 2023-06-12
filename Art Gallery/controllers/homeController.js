f238e81968f7113565c47f2a99e2fe163b64eaad
plamenpetroww <plamenpetroww@gmail.com>
2023-06-12 11:09:31 +0200

    add counter for shared

--- Art Gallery/controllers/homeController.js
+++ Art Gallery/controllers/homeController.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const artService = require('../services/artService');
 
-router.get('/', (req, res) => {
-    res.render('home');
+router.get('/', async (req, res) => {
+    const publicationResult = await artService.getAll();
+    const publications = publicationResult.map(x => ({ ...x, shareCount: x.usersShared.length }))
+    res.render('home', { publications });
 })
 
 module.exports = router;
