3fa29a40a2fc40e8a5077ac312cde415926c88f1
plamenpetroww <plamenpetroww@gmail.com>
2023-06-12 16:26:42 +0200

    make catalog page

--- Book Talk/controllers/bookController.js
+++ Book Talk/controllers/bookController.js
@@ -9,14 +9,19 @@ router.get('/create', (req, res) => {
 });
 
 router.post('/create', isAuth, async (req, res) => {
-    const bookData = {...req.body, author: req.user._id};
+    const bookData = { ...req.body, author: req.user._id };
 
     try {
         const book = await bookService.create(req.user._id, bookData);
         res.redirect('/book/catalog');
     } catch (error) {
-        res.render('book/create', {error: getErrorMessage(error), bookData})
+        res.render('book/create', { error: getErrorMessage(error), bookData })
     }
+});
+
+router.get('/catalog', async (req, res) => {
+    const book = await bookService.getAll()
+    res.render('book/catalog', { book })
 });
 
 module.exports = router;
