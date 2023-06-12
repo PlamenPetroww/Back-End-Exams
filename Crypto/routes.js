77dcdfed93ce8d49b95017607b78611b899f602d
plamenpetroww <plamenpetroww@gmail.com>
2023-06-12 11:14:47 +0200

    added all files

---
+++ Crypto/routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController');
+const cryptoController = require('./controllers/cryptoController');
+
+// TODO: add routes
+router.use(homeController);
+router.use(authController);
+router.all('*', (req, res) => {
+    res.render('home/404'); 
+});
+
+module.exports = router;
