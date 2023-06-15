b1f320504b8c73d46c19a05b1ec173b0a2bd52af
plamenpetroww <plamenpetroww@gmail.com>
2023-06-15 17:42:26 +0200

    add browse page

--- Auction House/controllers/auctionController.js
+++ Auction House/controllers/auctionController.js
@@ -4,10 +4,10 @@ const { isAuth } = require('../middlewares/authMiddleware');
 const { getErrorMessage } = require('../utils/errorUtils');
 
 router.get('/browse', async (req, res) => {
-    
-    const offers = await auctionService.getAll();
 
-    res.render('auction/browse', { offers })
+    const auctions = await auctionService.getAll();
+
+    res.render('auction/browse', { auctions })
 });
 
 router.get('/create', isAuth, async (req, res) => {
@@ -25,6 +25,11 @@ router.post('/create', isAuth, async (req, res) => {
     }
 });
 
+router.get('/:id/details', isAuth, async (req, res) => {
+    const offer = await auctionService.getOne(req.params.id);
+
+    res.render('auction/details', { offer });
+}),
 
 
-module.exports = router;
+    module.exports = router;
